b96d7468cbe2db05234780dc18666d99485c9aef
enzofragan <enzofragan@gmail.com>
2018-05-17 10:25:11 -0300

    asignar valor, mostrar direccion en exa en punteros

--- nomenclaturas/main.c
+++ nomenclaturas/main.c
@@ -7,24 +7,24 @@ int main()
 
     int *punteroNumero;
     int numero;
+    int numeroDos;
 
     numero = 66;
 
     punteroNumero=&numero;
+    numeroDos=&numero;///asig de memoria
+    printf("a- %d\n",numeroDos);
 
-    ///*punteroNumero=&numero;
-
-    ///*punteroNumero = 55;
+    numeroDos=*punteroNumero;///asig de valor
+    printf("b- %d\n",numeroDos);
 
     printf("%d\n",numero);
 
-    printf("%d\n",&numero);
-
-    printf("%d\n",punteroNumero);
-
-    printf("%d\n",&punteroNumero);
-
-   ///printf("%d",*punteroNumero);
+    printf("%d\n",&numero);///%p = notacion de punter o dericcion en exadecimal
+    printf(" d-%p\n",punteroNumero);///%p = notacion de punter o dericcion en exadecimal
+    printf("c-%p\n",&punteroNumero);///%p = notacion de punter o dericcion en exadecimal
+    printf(" d- %d\n",*punteroNumero);///valor puntero y da error cuando el puntero no tiene valor
+    printf("%d\n",numeroDos);
 
     return 0;
 }
