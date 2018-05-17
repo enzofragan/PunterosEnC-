d9d1836bad1bbf5db5cd3a46880c7e431b0f7330
enzofragan <enzofragan@gmail.com>
2018-05-17 10:38:16 -0300

    intercambio de valores con puneteros

---
+++ funcionesPunteros/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+void intercambiar(int num1, int num2);
+void intercambiarPunteros(int *num1, int *num2);
+int main()
+{
+    int edadUno;
+    int edadDos;
+    edadUno=22;
+    edadDos=99;
+    ///intercambiar(edadUno,edadDos);
+    intercambiarPunteros(&edadUno,&edadDos);
+    printf("\n edad 1:%d",edadUno);
+    printf("\n edad 2:%d",edadDos);
+
+    return 0;
+}
+void intercambiar(int num1, int num2)
+{
+    int aux;
+    aux=num1;
+    num1=num2;
+    num2=aux;
+}
+void intercambiarPunteros(int *num1, int *num2)
+{
+    int aux;
+    aux= *num1;
+    *num1=*num2;
+    *num2=aux;
+}
