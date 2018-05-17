e39223fe38da35cf73923e70295a34b0c61c63f1
enzofragan <enzofragan@gmail.com>
2018-05-17 11:59:35 -0300

    vectores y punteros

--- vectores/main.c
+++ vectores/main.c
@@ -4,10 +4,19 @@ void mostrar(int *puntero,int cantidad);
 int main()
 {
     int numeros[3];
+    int *puntero;
+    int auxiliar=333;
+    puntero=numeros;
 
     numeros[0]=9;
     numeros[1]=99;
     numeros[2]=999;
+    printf("\npuntero:%d",*(puntero-2));///mostrar array de numero sin for
+    printf("\npuntero:%d",*(puntero+1));
+    printf("\npuntero:%d",*(puntero+2));
+    printf("\npuntero:%d",*(puntero+4));///si sobre sale del arrays y accede al auxiliar
+    printf("\npuntero:%d",numeros);
+
     mostrar(numeros,3);
     return 0;
 }
@@ -16,6 +25,6 @@ void mostrar(int *puntero,int cantidad)
     int i;
     for(i=0;i<cantidad;i++)
     {
-        printf("\nnumero:%d",*(puntero+i));///*valor + 1 y sin * direccion de memoria
+        printf("\nnumero:%d",*(puntero+i));///*valor + 1 , sin * direccion de memoria + bits del numero y *()valora mas valor sig
     }
 }
