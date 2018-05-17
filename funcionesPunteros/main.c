75c1e0b81412ff3be928cb330430a19a87f8c10f
enzofragan <enzofragan@gmail.com>
2018-05-17 11:11:55 -0300

    vectores y cero con puenteros

--- funcionesPunteros/main.c
+++ funcionesPunteros/main.c
@@ -2,12 +2,15 @@
 #include <stdlib.h>
 void intercambiar(int num1, int num2);
 void intercambiarPunteros(int *num1, int *num2);
+void ponerEnCero(int *num1);
+
 int main()
 {
     int edadUno;
     int edadDos;
     edadUno=22;
     edadDos=99;
+    ponerEnCero(&edadDos);
     ///intercambiar(edadUno,edadDos);
     intercambiarPunteros(&edadUno,&edadDos);
     printf("\n edad 1:%d",edadUno);
@@ -28,4 +31,8 @@ void intercambiarPunteros(int *num1, int *num2)
     aux= *num1;
     *num1=*num2;
     *num2=aux;
+}
+void ponerEnCero(int *num1)
+{
+    *num1=0;
 }
