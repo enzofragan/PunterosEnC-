b2b8cad2f9c7ccf840a97fb4a9efe88e503dc850
enzofragan <enzofragan@gmail.com>
2018-05-18 10:13:44 -0300

    punteros con estructura, arrays y ->

--- estructuraPunteros/main.c
+++ estructuraPunteros/main.c
@@ -4,18 +4,38 @@ typedef struct
 {
     int a;//4
     char b;//1
-    char c[3];
-    int d;
 }dato;
 
 
 int main()
 {
-    dato d;
+    /**dato d;
     d.a=5;
     d.b='<';
 
-    printf("tam: %d\n",sizeof(dato));
-    printf("%d-------%c", d.a, d.b);
+    dato* pDato;
+
+    pDato=&d;///para direccion de menoria &
+
+    printf("%d--%c",pDato->a, pDato->b || (*pDato).a ,(*pDato).b);///-> se usa para evitar los (),* y el .
+    ///&pDato direccion de memoria, sin & la direccion que guarda, * valor de la direccion y (*). accesar al puntero que guarda la estructura y despues al campo
+*/
+
+
+    /**printf("tam: %d\n",sizeof(dato));
+    printf("%d-------%c", d.a, d.b);*/
+
+   int i;
+    dato l[2]={{1,'a'},{1,'b'}};
+    dato* pLista;
+
+    pLista = l;
+
+    for(i=0;i<2;i++)
+    {
+        printf("%d--%c\n",(pLista+i)->a ,(pLista+i)->b  /**(*(pLista+i)).a ,(*(pLista+i)).b*/);
+        ///(*(puntero+i)).a se usa para los datos de un array en estructuras
+    }
+
     return 0;
 }
