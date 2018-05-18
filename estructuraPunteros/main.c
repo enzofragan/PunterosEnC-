354fd58af294395f31f2380f1663fde184eecce9
enzofragan <enzofragan@gmail.com>
2018-05-18 09:41:42 -0300

    punteros con estructura

--- estructuraPunteros/main.c
+++ estructuraPunteros/main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+typedef struct
+{
+    int a;//4
+    char b;//1
+    char c[3];
+    int d;
+}dato;
+
 
 int main()
 {
-    printf("Hello world!\n");
+    dato d;
+    d.a=5;
+    d.b='<';
+
+    printf("tam: %d\n",sizeof(dato));
+    printf("%d-------%c", d.a, d.b);
     return 0;
 }
