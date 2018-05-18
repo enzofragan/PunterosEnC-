2dfe064fe2392f87aee8196417395c050df8e206
enzofragan <enzofragan@gmail.com>
2018-05-18 09:36:59 -0300

    punteros cargar mostrar

---
+++ punteros/main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int cargarArrays(int*,int);
+void mostrarArrays(int*,int);
+
+int main()
+{
+    int vector[3];
+    int* p;
+
+    if(cargarArrays(NULL,3))
+    {
+        printf("carga exitosa");
+    }
+    else
+    {
+        printf("error");
+    }
+    ///mostrarArrays(vector,3);
+
+    return 0;
+}
+
+int cargarArrays(int* array,int tam)
+{
+    int i;
+    int retorno=0;
+
+    if(array!=NULL)
+    {
+        retorno=1;
+        for(i=0;i<tam;i++)
+        {
+            printf("ingrese un numero: ");
+            scanf("%d",array+i);
+        }
+    }
+    return retorno;
+}
+
+void mostrarArrays(int* array,int tam)
+{
+    int i;
+
+    for(i=0;i<tam;i++)
+    {
+        printf("%d\n",*(array+i));
+    }
+}
